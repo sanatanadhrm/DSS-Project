d8bc2a30ab51e7acd518a73ea12a7df904d29edc
Natan <zantanad.24@gmail.com>
2024-06-18 17:30:33 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ export const HomePage = () => {
           parkir yang tersedia di lingkungan Universitas Pembangunan Nasional
           Veteran Jakarta
         </p>
-        <div className="flex justify-center h-full mt-10 md:mt-[70px]">
+        <div className="flex justify-center h-full mt-10 md:mt-[70px] gap-5">
           <Button
             type="primary"
             className="w-[150px] md:w-[200px] h-[50px] rounded-full text-lg font-semibold transform hover:scale-105 transition-transform duration-300"
@@ -29,6 +29,16 @@ export const HomePage = () => {
             }}
           >
             Mulai
+          </Button>
+          <Button
+            type="primary"
+            className="w-[150px] md:w-[200px] h-[50px] rounded-full text-lg font-semibold transform hover:scale-105 transition-transform duration-300"
+            style={{ backgroundColor: "#1890ff", borderColor: "#1890ff" }}
+            onClick={() => {
+              navigate("/admin");
+            }}
+          >
+            Set Parking - Admin
           </Button>
         </div>
       </div>
